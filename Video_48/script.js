92b461dc57beaea9c49006f42112ada695acf7a2
Thejas <thejasganiga150503@gmail.com>
2025-06-15 17:16:27 +0530

    Introduction To Javascript

--- Video_48/script.js
+++ Video_48/script.js
@@ -2,3 +2,22 @@ alert("Welcome To JavaScript Thejas !")
 console.log("Code is Running ... !")
 console.log("Code is also Running ... !")
 console.log("Code is looking like a Wow !")
+
+
+var a = prompt("Enter a Number : ")
+
+var isTrue = confirm("Do you want to leave the Page and Blast Your Computer !")
+
+if (isTrue) {
+    console.log("Blasting Computer !")
+}
+else {
+    console.log(
+        "Computer Not Blasting !"
+    )
+}
+console.log("Your Number is " + a)
+
+document.title = "Introduction To Javascript !"
+
+document.body.style.backgroundColor = "lightgreen";
