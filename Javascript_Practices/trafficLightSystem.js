5ca0929c0b60f5e53a0e0c42a560f15c936078c2
Thejas <thejasganiga150503@gmail.com>
2025-06-16 23:28:54 +0530

    Traffic Management System

---
+++ Javascript_Practices/trafficLightSystem.js
@@ -0,0 +1,20 @@
+// Traffic Light System
+
+let color = "green";
+
+if (color === "green") console.log("Go !");
+else if (color === "red") console.log("Stop !");
+else if (color === "Yellow") console.log("Get Ready !");
+else console.log("Invalid Color !");
+
+
+// Assigning two variables 
+
+let color1 = "green";
+
+let message;
+
+if (color1 === "red") message = "STOP !";
+else if (color1 === "green") message = "GO GO GO !";
+else if (color1 === "yellow") message = "GET READY !";
+console.log(message);
