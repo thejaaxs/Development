884e5d516921e3f569c6a436ceae838e2dc4915a
Thejas <thejasganiga150503@gmail.com>
2025-06-19 23:53:43 +0530

    Print Numbers from 1 to N

---
+++ Javascript_Practices/print1toN.js
@@ -0,0 +1,16 @@
+// Print Numbers from 1 to N
+
+let n = 10;
+
+for (let i = 0; i <= n; i++) {
+    console.log(`${i}`);
+}
+
+// Using Function 
+
+function print1ToN(n) {
+    for (let i = 0; i <= n; i++) {
+        console.log(i);
+    }
+}
+print1ToN(100);
