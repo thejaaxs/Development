db08732b53e8dcf1d7acb17360b1f2fde47e5601
Thejas <thejasganiga150503@gmail.com>
2025-07-04 23:08:16 +0530

    Array Practices

--- Javascript_Practices/arrayPractice2.js
+++ Javascript_Practices/arrayPractice2.js
@@ -115,3 +115,15 @@ function secondLargest(arr) {
     return unique[1];
 }
 console.log(secondLargest([10, 5, 20, 8, 20]));
+
+// Factorial 
+
+function factorial(n) {
+    let arr = Array.from(Array(n + 1).keys());
+    // console.log(arr.slice(1));
+    let c = arr.slice(1).reduce((a, b) => {
+        return a * b;
+    })
+    console.log(c);
+}
+factorial(5);
