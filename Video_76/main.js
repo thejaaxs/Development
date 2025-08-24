1be6e13f5f3e3c17e7a51d2f14a35c1f309cc709
Thejas <thejasganiga150503@gmail.com>
2025-08-24 23:26:56 +0530

    Introduction To Express.Js

---
+++ Video_76/main.js
@@ -0,0 +1,11 @@
+const { createServer } = require('node:http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('Hello <h1> Thejas </h1>');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
