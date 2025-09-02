85d0daffafb24bd6c14e8ef6b5ad0db731dbeec0
Thejas <thejasganiga150503@gmail.com>
2025-09-02 18:37:12 +0530

    Middleware In Express.Js

--- Video_78/main.js
+++ Video_78/main.js
@@ -1,19 +1,29 @@
 const express = require('express')
+const fs = require('fs')
+const blog = require('./routes/blog')
 const app = express()
 const port = 3000
 
 app.use(express.static('public'))
+app.use('/blog',blog)
 
 // the use of middleware 
+// adding a fs file to the text file
 
-app.use((req,res,next)=>{
-    console.log("MiddleWare-1")
-    next()
+app.use((req, res, next) => {
+    console.log(req.headers)
+    req.Thejas = "I Am Thejas";
+    fs.appendFileSync("Thejas.txt", `${Date.now()} is a ${req.method}\n`)
+    // console.log(`${Date.now()} is a ${req.method}`)
+    // console.log("MiddleWare-1")
+    next() // Commenting this makes the program end !
 })
 
 // but when you comment out the next() , then the program stucks in between !
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
+    // res.send("Hacked By MiddleWare 2"); // the program stops after this point !
+    req.Thejas = "I am Rahul"; // this will be printed !
     console.log("MiddleWare-2")
     next()
 })
@@ -23,7 +33,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.send("Helo About !")
+    res.send("Helo About ! " + `Its me ${req.Thejas}`)
 })
 
 app.get('/contact', (req, res) => {
