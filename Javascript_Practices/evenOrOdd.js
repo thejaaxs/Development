272bf54169549470a0e0ba850c4afb8df3063b7f
Thejas <thejasganiga150503@gmail.com>
2025-11-04 18:56:08 +0530

    Restart Back

--- Javascript_Practices/evenOrOdd.js
+++
@@ -1,9 +0,0 @@
-// Check if a number is even or odd
-
-let num = 7;
-if (num % 2 == 0) console.log(num + " is Even !");
-else console.log(num + " is Odd !");
-
-let num2 = 10;
-if (num2 ^ 1 == num2 + 1) console.log(num2, "is Even !");
-else if (num2 ^ 1 == num2 - 1) console.log(num2, "is Odd !");
--- Javascript_Practices/greetUser.js
+++
@@ -1,8 +0,0 @@
-// Greet user based on time
-
-const prompt = require('prompt-sync')(); // use the command npm install prompt-sync
-let hour = parseInt(prompt("Enter The Hour : "));
-
-if (hour < 12) console.log("Good Morning !");
-else if (hour < 18) console.log("Good Afternoon !");
-else console.log("Good Evening !");
